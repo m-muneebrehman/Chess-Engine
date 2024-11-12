7c6e8ee6706a8a39b86b745125638455f3c6ec79
bakhat123 <bakhatnasar246@gmail.com>
2024-11-13 00:35:09 +0500

    Secondary push

--- main.cpp
+++ main.cpp
@@ -11,11 +11,11 @@ int main() {
     Bitboard allPieces = 0ULL;
 
     // Initialize white pawns on row 2
-    for (int i = A2; i <= H2; ++i) {
-        whitePawns = set_bit(whitePawns, i);
-    }
+    // for (int i = A2; i <= H2; ++i) {
+    //     whitePawns = set_bit(whitePawns, i);
+    // }
 
-    // Initialize black pawns on row 7
+    //Initialize black pawns on row 7
     for (int i = A7; i <= H7; ++i) {
         blackPawns = set_bit(blackPawns, i);
     }
@@ -31,14 +31,21 @@ int main() {
 
     // Initialize a knight at B1 (square index 1)
     Bitboard knight = 0ULL;
-    knight = set_bit(knight, G1);
+    knight = set_bit(knight, B1);
 
     // Test knight move generation
-    cout << "Knight Moves from G1:" << endl;
+    cout << "Knight Moves from B1:" << endl;
     Bitboard knightMoves = generate_knight_moves(knight, allPieces);
     print_bitboard(knightMoves);
 
-    // Test sliding pieces later (bishop/rook/queen) after you confirm these work
+    // Initialize a rook at A1 (square index 0)
+    Bitboard rook = 0ULL;
+    rook = set_bit(rook, A1);
+
+    // Test rook move generation
+    cout << "Rook Moves from A1:" << endl;
+    Bitboard rookMoves = generate_sliding_moves(rook, allPieces, 8);
+    print_bitboard(rookMoves);
 
     return 0;
 }
